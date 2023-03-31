128577ddb416509e264d54e68742eb84272ba1b1
Justin Lee <lee.justin001126@gmail.com>
2023-03-31 11:22:40 -0700

    Add delte fucntionaltiy, run error -532462766, WIP fixing bug

--- AnimalShelter/Controllers/AnimalsController.cs
+++ AnimalShelter/Controllers/AnimalsController.cs
@@ -73,5 +73,20 @@ namespace AnimalShelter.Controllers
       {
         return _db.Animals.Any(obj => obj.AnimalId == id);
       }
+
+      [HttpDelete("{id}")]
+      public async Task<IActionResult> DeleteAnimal(int id)
+      {
+        Animal animal = await _db.Animals.FindAsync(id);
+        if(animal == null)
+        {
+          return NotFound();
+        }
+
+        _db.Animals.Remove(animal);
+        await _db.SaveChangesAsync();
+        
+        return NoContent();
+      }
     }
   }
