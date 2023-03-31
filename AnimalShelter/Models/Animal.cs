7523253bee37fc06861d999cbd0e25f14a6d48ed
Justin Lee <lee.justin001126@gmail.com>
2023-03-31 08:39:46 -0700

    Working on more boiler plat files WIP

---
+++ AnimalShelter/Models/Animal.cs
@@ -0,0 +1,11 @@
+namespace AnimalShelter.Models
+{
+  public class Animal
+  {
+    public int AnimalId { get; set; }
+    public string Breed { get; set; }
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string Color { get; set; }
+  }
+}
